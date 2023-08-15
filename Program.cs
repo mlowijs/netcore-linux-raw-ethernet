95429a7b32f418f5b18201c25db0770b294b3f2b
Michiel Lowijs <mlowijs@gmail.com>
2023-08-15 16:42:33 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -1,19 +1,16 @@
 ﻿using System.Net.NetworkInformation;
 
-const string networkInterfaceName = "eth0";
+const string networkInterfaceName = "enp61s0"; // eth0
 
 var networkInterface = NetworkInterface.GetAllNetworkInterfaces().First(ni => ni.Name == networkInterfaceName);
-var destinationMac = new byte[]
-{
-    0x00, 0x0d, 0xb9, 0x49, 0x15, 0x6d
-};
+var destinationMac = new byte[] { 0x00, 0x0d, 0xb9, 0x49, 0x15, 0x6d };
 
 var endpoint = new LinuxEthernetEndPoint(networkInterface, destinationMac);
 
 using var frame = new MemoryStream();
 frame.Write(destinationMac); // Destination MAC
 frame.Write(networkInterface.GetPhysicalAddress().GetAddressBytes()); // Source MAC
-frame.Write(new byte[] { 0x08, 0x00 }); // EtherType
+frame.Write(new byte[] { 0x08, 0x00 }); // EtherType (IP)
 frame.Write(new byte[] { 0xde, 0xad, 0xbe, 0xef }); // Payload
 
 using var sock = new LinuxRawSocket();
